6317d65b297b33024e2a45351f93e5d1455387ac
danielocasio03 <64695744+danielocasio03@users.noreply.github.com>
2024-09-04 21:40:13 -0400

    Initial Commit

---
+++ Foody/HomeScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Daniel Efrain Ocasio on 9/4/24.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
